11fa5ab1c8cd06c166e281738a7b0e2c2ed3253f
esvhd <7330385+esvhd@users.noreply.github.com>
2019-08-26 22:53:15 +0100

    placeholder for ts transformer

--- former/transformers.py
+++ former/transformers.py
@@ -133,3 +133,84 @@ class CTransformer(nn.Module):
         x = self.toprobs(x)
 
         return F.log_softmax(x, dim=1)
+
+
+class TSTransformer(nn.Module):
+    """
+    Transformer for classifying sequences
+    """
+    def __init__(
+        self,
+        # emb,
+        heads,
+        depth,
+        seq_length,
+        # num_tokens,
+        num_classes,
+        max_pool=True,
+        dropout=0.0,
+    ):
+        """
+        :param emb: Embedding dimension
+        :param heads: nr. of attention heads
+        :param depth: Number of transformer blocks
+        :param seq_length: Expected maximum sequence length
+        # :param num_tokens: Number of tokens (usually words) in the vocabulary
+        :param num_classes: Number of classes.
+        :param max_pool: If true, use global max pooling in the last layer. If false, use global
+                         average pooling.
+        """
+        super().__init__()
+
+        self.num_tokens, self.max_pool = num_tokens, max_pool
+
+        # don't need token embedding as input is numerical, but with dim > 1
+        # self.token_embedding = nn.Embedding(
+        #     embedding_dim=emb, num_embeddings=num_tokens
+        # )
+        # how to use positional embedding?
+        self.pos_embedding = nn.Embedding(
+            embedding_dim=emb, num_embeddings=seq_length
+        )
+
+        tblocks = []
+        for i in range(depth):
+            tblocks.append(
+                TransformerBlock(
+                    emb=emb,
+                    heads=heads,
+                    seq_length=seq_length,
+                    mask=False,
+                    dropout=dropout,
+                )
+            )
+
+        self.tblocks = nn.Sequential(*tblocks)
+
+        self.toprobs = nn.Linear(emb, num_classes)
+
+        self.do = nn.Dropout(dropout)
+
+    def forward(self, x):
+        """
+        :param x: A batch by sequence length integer tensor of token indices.
+        :return: predicted log-probability vectors for each token based on the preceding tokens.
+        """
+        tokens = self.token_embedding(x)
+        b, t, e = tokens.size()
+
+        positions = self.pos_embedding(torch.arange(t, device=d()))[
+            None, :, :
+        ].expand(b, t, e)
+        x = tokens + positions
+        x = self.do(x)
+
+        x = self.tblocks(x)
+
+        x = (
+            x.max(dim=1)[0] if self.max_pool else x.mean(dim=1)
+        )  # pool over the time dimension
+
+        x = self.toprobs(x)
+
+        return F.log_softmax(x, dim=1)
