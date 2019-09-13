e7e4dbdef823b3f008f78d1563287f845ea8b975
esvhd <7330385+esvhd@users.noreply.github.com>
2019-09-12 21:58:56 -0400

    ts regression transformer

--- former/transformers.py
+++ former/transformers.py
@@ -143,14 +143,14 @@ class TSTransformer(nn.Module):
     def __init__(
         self,
         # emb,
-        feature_dim,
-        heads,
-        depth,
-        seq_length,
+        feature_dim: int,
+        heads: int,
+        depth: int,
+        seq_length: int,
         # num_tokens,
-        num_classes,
-        max_pool=True,
-        dropout=0.0,
+        num_classes: int,
+        max_pool: bool = True,
+        dropout: float = 0.0,
     ):
         """
         A time series transfomrmer that can handle both numerical and
@@ -223,3 +223,93 @@ class TSTransformer(nn.Module):
         x = self.toprobs(x)
 
         return F.log_softmax(x, dim=1)
+
+
+class TSRegTransformer(nn.Module):
+    """
+    Transformer for regression problems.
+    """
+    def __init__(
+        self,
+        # emb,
+        feature_dim: int,
+        heads: int,
+        depth: int,
+        seq_length: int,
+        # num_tokens,
+        # num_classes: int,
+        out_dim: int,
+        max_pool: bool = True,
+        dropout: float = 0.0,
+    ):
+        """
+        A time series transfomrmer that can handle both numerical and
+        categorical features. Cat features are converted into entity embeddings.
+
+        :param emb: Embedding dimension
+        :param heads: nr. of attention heads
+        :param depth: Number of transformer blocks
+        :param seq_length: Expected maximum sequence length
+        # :param num_tokens: Number of tokens (usually words) in the vocabulary
+        :param out_dim: regression output dimension.
+        :param max_pool: If true, use global max pooling in the last layer.
+            If false, use global
+                         average pooling.
+        """
+        super().__init__()
+
+        # self.num_tokens = num_tokens
+        self.max_pool = max_pool
+
+        # don't need token embedding as input is numerical, but with dim > 1
+        # self.token_embedding = nn.Embedding(
+        #     embedding_dim=emb, num_embeddings=num_tokens
+        # )
+        # how to use positional embedding?
+        self.pos_embedding = nn.Embedding(
+            embedding_dim=feature_dim, num_embeddings=seq_length
+        )
+
+        tblocks = []
+        for i in range(depth):
+            tblocks.append(
+                TransformerBlock(
+                    emb=feature_dim,
+                    heads=heads,
+                    seq_length=seq_length,
+                    mask=True,
+                    dropout=dropout,
+                )
+            )
+
+        self.tblocks = nn.Sequential(*tblocks)
+
+        self.linear_output = nn.Linear(feature_dim, out_dim)
+
+        self.do = nn.Dropout(dropout)
+
+    def forward(self, x):
+        """
+        :param x: A batch by sequence length integer tensor of token indices.
+        :return: predicted log-probability vectors for each token based on the
+        preceding tokens.
+        """
+        # tokens = self.token_embedding(x)
+        # b, t, e = tokens.size()
+        b, t, e = x.shape
+
+        positions = self.pos_embedding(torch.arange(t, device=d()))[
+            None, :, :
+        ].expand(b, t, e)
+        x = x.float() + positions
+        x = self.do(x)
+
+        x = self.tblocks(x)
+
+        x = (
+            x.max(dim=1)[0] if self.max_pool else x.mean(dim=1)
+        )  # pool over the time dimension
+
+        x = self.linear_output(x)
+
+        return x
